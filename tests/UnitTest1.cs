cc318ac0934d925951905305666df59d132aaab7
Dave Farinelli <d@dfar.io>
2021-01-11 13:37:29 -0500

    Separated to src/ tests/ folders

---
+++ tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Nop.Plugin.Misc.FreshAddressIntegration.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
