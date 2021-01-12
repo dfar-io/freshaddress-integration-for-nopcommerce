e0e4475d5aa37cb30401f8e14caf029066fea87e
Dave Farinelli <d@dfar.io>
2021-01-12 10:25:49 -0500

    Added test project

--- sonar-helper.sh
+++ sonar-helper.sh
@@ -1,7 +1,7 @@
 # Marks all non-plugin .NET projects as skipped for Sonar
 # to clean up scan results
 
-for file in $(find ../.. -type f -name "*.csproj" -not -path "../../Plugins/Nop.Plugin.Misc.FreshAddressIntegration/Source/Nop.Plugin.Misc.FreshAddressIntegration.csproj")
+for file in $(find ../.. -type f -name "*.csproj" -not -path "../../Plugins/Nop.Plugin.Misc.FreshAddressIntegration/Source/Nop.Plugin.Misc.FreshAddressIntegration.csproj" -not -path "../../Plugins/Nop.Plugin.Misc.FreshAddressIntegration/Tests/Nop.Plugin.Misc.FreshAddressIntegration.Tests.csproj")
 do
     echo "processing $file"
     sed -i -- 's/<\/Project>//' $file > /dev/null
