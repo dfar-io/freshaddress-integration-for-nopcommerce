3b03140fc0d7347df7c7f9ba6a3e69f74f53a388
Dave Farinelli <d@dfar.io>
2021-01-17 10:42:31 -0500

    Subscribe email test

--- Tests/CustomNewsletterControllerTests.cs
+++ Tests/CustomNewsletterControllerTests.cs
@@ -3,7 +3,10 @@ using Microsoft.AspNetCore.Mvc;
 using Moq;
 using Nop.Core;
 using Nop.Core.Domain.Configuration;
+using Nop.Core.Domain.Localization;
+using Nop.Core.Domain.Stores;
 using Nop.Plugin.Misc.FreshAddressIntegration.Controllers;
+using Nop.Plugin.Misc.FreshAddressIntegration.Models;
 using Nop.Plugin.Misc.FreshAddressIntegration.Services;
 using Nop.Services.Configuration;
 using Nop.Services.Localization;
@@ -18,6 +21,7 @@ namespace Nop.Plugin.Misc.FreshAddressIntegration.Tests
         private CustomNewsletterController _controller;
 
         private const string InvalidEmail = "Invalid email.";
+        private const string SubscribeEmailSent = "Subscribe email sent.";
 
         [SetUp]
         public void Setup()
@@ -25,15 +29,32 @@ namespace Nop.Plugin.Misc.FreshAddressIntegration.Tests
             var localizationService = new Mock<ILocalizationService>();
             localizationService.Setup(s => s.GetResource("Newsletter.Email.Wrong"))
                                .Returns(InvalidEmail);
+            localizationService.Setup(s => s.GetResource("Newsletter.SubscribeEmailSent"))
+                               .Returns(SubscribeEmailSent);
+
+            var workContext = new Mock<IWorkContext>();
+            workContext.SetupGet(c => c.WorkingLanguage)
+                       .Returns(new Language(){ Id = 1 });
+
+            var storeContext = new Mock<IStoreContext>();
+            storeContext.SetupGet(c => c.CurrentStore).Returns(new Store() { Id = 1 });
+
+            var freshAddressService = new Mock<IFreshAddressService>();
+            freshAddressService.Setup(s => s.ValidateEmail(It.IsAny<string>()))
+                               .Returns(
+                                   new FreshAddressResponse() {
+                                       Finding = "V"
+                                   }
+                               );
 
             _controller = new CustomNewsletterController(
                 localizationService.Object,
-                new Mock<IWorkContext>().Object,
+                workContext.Object,
                 new Mock<INewsLetterSubscriptionService>().Object,
                 new Mock<IWorkflowMessageService>().Object,
-                new Mock<IStoreContext>().Object,
+                storeContext.Object,
                 new Mock<ILogger>().Object,
-                new Mock<IFreshAddressService>().Object
+                freshAddressService.Object
             );
         }
 
@@ -42,6 +63,14 @@ namespace Nop.Plugin.Misc.FreshAddressIntegration.Tests
         {
             _controller.SubscribeNewsletter("abc", true).Should().BeEquivalentTo(
                 new JsonResult(new { Success = false, Result = InvalidEmail })
+            );
+        }
+
+        [Test]
+        public void Send_Subscribe_Email()
+        {
+            _controller.SubscribeNewsletter("a@a.com", true).Should().BeEquivalentTo(
+                new JsonResult(new { Success = true, Result = SubscribeEmailSent })
             );
         }
     }
