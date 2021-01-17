289896acdda7c195f2e2adc5cc12fbe673e09ea2
Dave Farinelli <d@dfar.io>
2021-01-17 09:22:35 -0500

    Attempting code coverage exclusion

--- Source/Infrastructure/DependencyRegistrar.cs
+++ Source/Infrastructure/DependencyRegistrar.cs
@@ -3,9 +3,11 @@ using Autofac;
 using Nop.Core.Configuration;
 using Nop.Core.Infrastructure;
 using Nop.Plugin.Misc.FreshAddressIntegration.Services;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Nop.Plugin.Misc.FreshAddressIntegration.Infrastructure
 {
+    [ExcludeFromCodeCoverage]
     public class DependencyRegistrar : IDependencyRegistrar
     {
         public int Order
